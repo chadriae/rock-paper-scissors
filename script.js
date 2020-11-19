83b41ec9a645c027597d78c213afbe329210b0b4
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2020-11-19 14:31:03 +0100

    Jss code cleanup

--- script.js
+++ script.js
@@ -24,45 +24,41 @@ document.getElementById('play').addEventListener("click", function () {
     const randomNumber = Math.floor(Math.random() * 3);
     const rps = ["rock", "paper", "scissors"];
 
-        // Declaring all the outcomes
-        if (choice == rps[randomNumber]) {
-        document.getElementById("result").innerHTML = "It's a draw!";
-        draw ++;
-        }
+    // Declaring all the outcomes
+    if (choice == rps[randomNumber]) {
+    document.getElementById("result").innerHTML = "It's a draw!";
+    draw ++;
+    }
 
-        else if (choice == 'paper' && randomNumber === 0) {
-        document.getElementById("result").innerHTML = "You won!";
-        persScore ++;
-        }
+    else if (choice == 'paper' && randomNumber === 0) {
+    document.getElementById("result").innerHTML = "You won!";
+    persScore ++;
+    }
 
-        else if (choice == 'scissors' && randomNumber === 1) {
-        document.getElementById("result").innerHTML = "You won!";
-        persScore ++;
-        }
+    else if (choice == 'scissors' && randomNumber === 1) {
+    document.getElementById("result").innerHTML = "You won!";
+    persScore ++;
+    }
 
-        else if (choice == 'rock' && randomNumber === 2) {
-        document.getElementById("result").innerHTML = "You won!";
-        persScore ++;
-        }
+    else if (choice == 'rock' && randomNumber === 2) {
+    document.getElementById("result").innerHTML = "You won!";
+    persScore ++;
+    }
 
-        else {
-        document.getElementById("result").innerHTML = "You lost.";
-        pcScore ++;
-        }
+    else {
+    document.getElementById("result").innerHTML = "You lost.";
+    pcScore ++;
+    }
 
-        // Changing all html-elements with scores etc
-        document.getElementById("choices").innerHTML = "You chose " + choice + " and the computer chose " + rps[randomNumber] + ".";
-        document.getElementById("yourScore").innerHTML = "Your score: " + persScore.toString() + ".";
-        document.getElementById("pcScore").innerHTML = "Your opponents score: " + pcScore.toString() + ".";  
-        document.getElementById("test").innerHTML = "Number of draws: " + draw.toString() + ".";  
+    // Changing all html-elements with scores etc
+    document.getElementById("choices").innerHTML = "You chose " + choice + " and the computer chose " + rps[randomNumber] + ".";
+    document.getElementById("yourScore").innerHTML = "Your score: " + persScore.toString() + ".";
+    document.getElementById("pcScore").innerHTML = "Your opponents score: " + pcScore.toString() + ".";  
+    document.getElementById("test").innerHTML = "Number of draws: " + draw.toString() + ".";  
 
 
-        // Changing the fight-button innerHTML -> not working?
-        document.getElementById("play").innerHTML = 'PLAY AGAIN? &#x2694;';
+    // Changing the fight-button innerHTML
+    document.getElementById("play").innerHTML = 'PLAY AGAIN? &#x2694;';
         
 })
-
-        
-
-
 })();
