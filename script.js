5b4572a3c6a9fe054a30f25091efdc7408f624ee
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2020-11-19 12:05:01 +0100

    Add javascript comments

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 (() => {
+// Declaring variables
 let persScore = 0;
 let pcScore = 0;
 let draw = 0;
 let choice = '';
 
+
+// Getting the value of the clicked button and adding as an array to let choice
 Array.from(document.querySelectorAll('button')).forEach(($btn) =>
     $btn.addEventListener(
       'click',
@@ -13,14 +16,16 @@ Array.from(document.querySelectorAll('button')).forEach(($btn) =>
       false
 ))
 
+
+// Starting the function after clicking "fight"
+// Using the variable choice to determine who wins
 document.getElementById('fight').addEventListener("click", function () {
 
-    console.log(choice);
+    // Randomising an outcome of the opponent
+    const randomNumber = Math.floor(Math.random() * 3);
+    const rps = ["rock", "paper", "scissors"];
 
-        const randomNumber = Math.floor(Math.random() * 3);
-        const rps = ["rock", "paper", "scissors"];
-
-
+        // Declaring all the outcomes
         if (choice == rps[randomNumber]) {
         document.getElementById("result").innerHTML = "It's a draw!";
         draw ++;
@@ -46,6 +51,7 @@ document.getElementById('fight').addEventListener("click", function () {
         pcScore ++;
         }
 
+        // Changing all html-elements with scores etc
         document.getElementById("choices").innerHTML = "You chose " + choice + " and the computer chose " + rps[randomNumber] + ".";
         document.getElementById("yourScore").innerHTML = "Your score: " + persScore.toString() + ".";
         document.getElementById("pcScore").innerHTML = "Your opponents score: " + pcScore.toString() + ".";  
