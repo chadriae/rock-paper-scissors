015c3bbb4afea93a764f93bd7317a13c6f68a307
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2020-11-19 11:51:51 +0100

    Fully working game with added styling

--- script.js
+++ script.js
@@ -1,44 +1,54 @@
+(() => {
 let persScore = 0;
 let pcScore = 0;
+let draw = 0;
+let choice = '';
 
-function rockPaperScissors(choice) {
+Array.from(document.querySelectorAll('button')).forEach(($btn) =>
+    $btn.addEventListener(
+      'click',
+      () => {
+        choice = $btn.id;
+      },
+      false
+))
 
-    const randomNumber = Math.floor(Math.random() * 3);
-    const rps = ["rock", "paper", "scissors"];
+document.getElementById('fight').addEventListener("click", function () {
 
+    console.log(choice);
 
-    if (choice == rps[randomNumber]) {
-    document.getElementById("result").innerHTML = "It's a draw!";
-    }
-
-    else if (choice == 'paper' && randomNumber === 0) {
-    document.getElementById("result").innerHTML = "You won!";
-    persScore ++;
-    }
-
-    else if (choice == 'scissors' && randomNumber === 1) {
-    document.getElementById("result").innerHTML = "You won!";
-    persScore ++;
-    }
-
-    else if (choice == 'rock' && randomNumber === 2) {
-    document.getElementById("result").innerHTML = "You won!";
-    persScore ++;
-    }
-
-    else {
-    document.getElementById("result").innerHTML = "You lost.";
-    pcScore ++;
-    }
-
-    console.log(randomNumber);
-    console.log(rps[randomNumber]);
-    console.log(persScore);
-    console.log(pcScore);
-
-    document.getElementById("choices").innerHTML = "You chose " + choice + " and the computer chose " + rps[randomNumber] + ".";
-    document.getElementById("yourScore").innerHTML = "Your score: " + persScore.toString() + ".";
-    document.getElementById("pcScore").innerHTML = "Your opponents score: " + pcScore.toString() + ".";  
-    };
+        const randomNumber = Math.floor(Math.random() * 3);
+        const rps = ["rock", "paper", "scissors"];
 
 
+        if (choice == rps[randomNumber]) {
+        document.getElementById("result").innerHTML = "It's a draw!";
+        draw ++;
+        }
+
+        else if (choice == 'paper' && randomNumber === 0) {
+        document.getElementById("result").innerHTML = "You won!";
+        persScore ++;
+        }
+
+        else if (choice == 'scissors' && randomNumber === 1) {
+        document.getElementById("result").innerHTML = "You won!";
+        persScore ++;
+        }
+
+        else if (choice == 'rock' && randomNumber === 2) {
+        document.getElementById("result").innerHTML = "You won!";
+        persScore ++;
+        }
+
+        else {
+        document.getElementById("result").innerHTML = "You lost.";
+        pcScore ++;
+        }
+
+        document.getElementById("choices").innerHTML = "You chose " + choice + " and the computer chose " + rps[randomNumber] + ".";
+        document.getElementById("yourScore").innerHTML = "Your score: " + persScore.toString() + ".";
+        document.getElementById("pcScore").innerHTML = "Your opponents score: " + pcScore.toString() + ".";  
+        document.getElementById("draw").innerHTML = "Number of draws: " + draw.toString() + ".";  
+    })
+})();
